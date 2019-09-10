05ed0597d4f8deab9ae5a4080e211ee0c9ff2514
chzh1418 <chzh1418@colorado.edu>
2019-09-10 05:23:39 +0000

    first trial

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,15 +1,31 @@
-import sys, math
+import sys
+import math
+import argparse
 
 file_name = sys.argv[1]
 col_num = int(sys.argv[2])
 
-f = open(file_name, 'r')
+parser = argparse.ArgumentParser(
+            description='Pass Parameters')
+
+parser.add_argument('--file_name',
+                    type=str,
+                    help='Name of the file',
+                    required=True)
+parser.add_argument('--col_num',
+                    type=int,
+                    help='Number of column',
+                    required=True)
+
+args = parser.parse_args()
+
+f = open(args.file_name, 'r')
 
 V = []
 
 for l in f:
     A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+    V.append(A[args.col_num])
 
 mean = sum(V)/len(V)
 
