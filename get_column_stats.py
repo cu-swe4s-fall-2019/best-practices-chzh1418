223dc3cb076a108d6124d09bf1e87e0901eb1ffd
chzh1418 <chzh1418@colorado.edu>
2019-09-24 18:42:07 +0000

    modify get_colun_stats.py

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,9 +1,28 @@
 import sys
 import math
 import argparse
-# parsing arguments
 
 
+# Define function to calculate mean of a vector
+def get_mean(V):
+    if len(V) == 0:
+        raise ValueError('get_mean, empty input')
+        sys.exit(1)
+    mean = sum(V)/len(V)
+    return mean
+
+
+# Define functions to calculate standard deviation of a vector
+def get_stdev(V):
+    if len(V) == 0:
+        raise ValueError('get_stdev, empty input')
+        sys.exit(1)
+    mean = get_mean(V)
+    stdev = math.sqrt(sum([(mean - x)**2 for x in V]))
+    return stdev
+
+
+# Parsing arguments using argparse
 def arguments():
     parser = argparse.ArgumentParser(
             description='Pass Parameters')
@@ -17,49 +36,46 @@ def arguments():
                         help='Number of column',
                         required=True)
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
+
+# Main function to incorprate all the data and outputs
+def get_stats():
+    args = arguments()
     file_name = args.file_name
     col_num = args.col_num
+    mean = None
+    stdev = None
+    V = []
+# check if the file exit
+    try:
+        input_file = open(file_name, 'r')
+    except FileNotFoundError:
+        print('Did not find input file')
+        sys.exit(1)
 
 
-def get_mean(V):
-    mean = sum(V)/len(V)
-    return mean
+# check the colnum number is valid
+    if col_num < 0:
+        raise ValueError('Column number not correct')
+        sys.exit(1)
 
-
-def get_stdev(V):
-    mean = get_mean(V)
-    stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
-    return stdev
+    for row in input_file:
+        try:
+            A = [int(x) for x in row.split()]
+            V.append(A[col_num])
+        except ValueError:
+            print('Input file issue')
+            sys.exit(1)
+    if len(V) == 0:
+        raise Exception('length of column is zero')
+        sys.exit(1)
+    else:
+        mean = get_mean(V)
+        stdev = get_stdev(V)
+    print('mean', mean)
+    print('stdev', stdev)
 
 
 if __name__ == "__main__":
-    args = arguments()
-    try:
-        file = open(args.file_name, 'r')
-    except FileNotFoundError as error:
-        print('FileNotFoundError: {}'.format(error))
-        sys.exit(1)
-
-    V = []
-    mean = None
-    stdev = None
-
-    for line in file:
-        A = [int(x) for x in line.split()]
-        try:
-            V.append(A[args.col_num])
-        except IndexError as error:
-            print('IndexError: {}'.format(error))
-            sys.exit(1)
-
-    # calculate result
-    mean = sum(V) / len(V)
-    stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
-
-    # print result
-    print('mean:', mean)
-    print('stdev:', stdev)
-# modify the get column stats file to be testable for the unit test
-# mainly by make functions available to run and return
+    get_stats()
